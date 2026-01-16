9270fac21574c2bdb63076bf731380feb49c7d13
noonnoo <78407666+noonnoonanna@users.noreply.github.com>
2026-01-16 18:00:12 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -3,12 +3,12 @@ const supabaseClient = supabase.createClient(
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxmbG5zc3d2bHhqcGlrcGhkY3h6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc5MjQ5NjMsImV4cCI6MjA4MzUwMDk2M30.QqoOA6qfv1fU11OMWl3vBw9EnZjmisSqK1Y0SO7ij0E'
 );
 
+ let valid = false;
+  
 (function () {
   const LIMIT = 1000 * 60 * 60 * 2; // 2시간
   const loginTime = sessionStorage.getItem('admin_login_time');
   const isLoggedIn = sessionStorage.getItem('admin_logged_in') === 'true';
-
-  let valid = false;
 
   if (isLoggedIn && loginTime) {
     if (Date.now() - Number(loginTime) <= LIMIT) {
